9c3f6bb487c6cd274240ec001b50757efbbc75e5
Lucas Esteves <lucasesteves5304@gmail.com>
2021-11-24 12:59:18 -0300

    Criando components functional

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FormularioCadastro() {
+    return (
+    
+        <div >
+            
+<h1 >0</h1>
+<button>Clique me</button>
+        </div>
+    
+    );
+}
+
+export default FormularioCadastro;
