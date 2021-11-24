9222bbed288808121ad3de6f89f62cbb42d4d63d
Lucas Esteves <lucasesteves5304@gmail.com>
2021-11-24 17:28:16 -0300

    terminando visual com framework

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import {Button,TextField,Switch,FormControlLabel } from '@material-ui/core';
+import { Button, TextField, Switch, FormControlLabel,Typography  } from '@material-ui/core';
 
 function FormularioCadastro() {
     return (
-<>
-    <form>
+        <>
+            <form>
 
-    <h1 >formulario de cadastro</h1>
+                <Typography align='center' variant="h3" component="h1" >Formulario de Cadastro</Typography>
 
-    <TextField id="nome" label="Nome:" variant="outlined"  margin="normal" fullWidth />
+                <TextField id="nome" label="Nome:" variant="outlined" margin="normal" fullWidth />
 
-    <TextField id="nome" label="Sobrenome:" variant="outlined"  margin="normal" fullWidth />
+                <TextField id="nome" label="Sobrenome:" variant="outlined" margin="normal" fullWidth />
 
-    <TextField id="cpf" label="Cpf:" variant="outlined"  margin="normal" fullWidth />
+                <TextField id="cpf" label="Cpf:" variant="outlined" margin="normal" fullWidth />
+
+                <FormControlLabel label="Promoções" control={<Switch nome="Promo" defaultChecked color="primary" />} />
+
+                <FormControlLabel label="Novidades" control={<Switch nome="Novidades" defaultChecked color="primary" />} />
 
 
 
-    <FormControlLabel label="Novidades"  control={<Switch nome="Novidades" defaultChecked color="primary" />} />
+                <Button variant="contained" color="primary">
+                    CADASTRAR
+                </Button>
+            </form>
+        </>
 
-
- 
-
-    
-        <Button variant="contained" color="primary">
-      Hello World   
-    </Button>
-    </form>
- </>
-    
     );
 }
 
