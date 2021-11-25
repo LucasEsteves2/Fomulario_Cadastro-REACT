299a58f7fc530688cc684e582d202fc790aa3ed2
Lucas Esteves <lucasesteves5304@gmail.com>
2021-11-24 23:36:33 -0300

    utilizando state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import Container from '@material-ui/core/Container'
 
+
 class App extends React.Component {
   render() { 
     return ( 
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Button, TextField, Switch, FormControlLabel,Typography  } from '@material-ui/core';
 
+
 function FormularioCadastro() {
+
+    const [usuario,setUsuario] = useState("")
+    const [cpf,setCpf]=useState();
+    const [sobrenome,setSobrenome]=useState();
+
+
     return (
         <>
-            <form>
+            <form onSubmit={(event) => {
+                
+                event.preventDefault()
+                console.log(usuario)
+                console.log(sobrenome)
+                console.log(cpf)
+
+              }}>
 
                 <Typography align='center' variant="h3" component="h1" >Formulario de Cadastro</Typography>
 
-                <TextField id="nome" label="Nome:" variant="outlined" margin="normal" fullWidth />
+                <TextField id="usuario" label="Nome:" variant="outlined" margin="normal" fullWidth  value={usuario}
+                 onChange={(event) => {
+                    setUsuario(event.target.value);
+                
+                    if(usuario.length >= 3)
+                    {
+                        setUsuario(usuario.substr(0,3));
+                        }
 
-                <TextField id="nome" label="Sobrenome:" variant="outlined" margin="normal" fullWidth />
+                }} />
 
-                <TextField id="cpf" label="Cpf:" variant="outlined" margin="normal" fullWidth />
+                <TextField id="nome" label="Sobrenome:" variant="outlined" margin="normal" fullWidth 
+                onChange={ (event) => {
+                    setSobrenome(event.target.value)
+                }}
+                
+                />
+
+                <TextField id="cpf" label="Cpf:" variant="outlined" margin="normal" fullWidth 
+                onChange={(event)=>
+                {
+                    setCpf( event.target.value)
+                }}
+                
+                />
 
                 <FormControlLabel label="Promoções" control={<Switch nome="Promo" defaultChecked color="primary" />} />
 
@@ -20,9 +54,12 @@ function FormularioCadastro() {
 
 
 
-                <Button variant="contained" color="primary">
+        
+                <Button variant="contained" color="primary" type="submit" >
                     CADASTRAR
                 </Button>
+
+                
             </form>
         </>
 
